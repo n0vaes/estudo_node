8f36dcbc6c876148335421bd81ebf8e05a04cb97
alan <alanovaes@live.com>
2022-03-16 09:16:46 -0300

    Início typescript

---
+++ typescript/dist/CreateCourseService.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * name - string
+ * duration - number
+ * educator - string
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+class CreateCourseService {
+    execute({ duration, educator, name }) {
+        console.log(name, duration, educator);
+    }
+    ;
+}
+;
+exports.default = new CreateCourseService();
